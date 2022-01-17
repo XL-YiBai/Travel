3e80f3da992ce18c8cf17d0bb12f42a8788a3451
leishao06 <987313405@qq.com>
2022-01-17 23:35:55 +0800

    修复问题

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import fastclick from 'fastclick'
 // 引入VueAwesomeSwiper轮播图插件库
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import store from './store/index'
@@ -13,7 +12,6 @@ import 'styles/iconfont.css'
 import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
-fastclick.attach(document.body)
 Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
